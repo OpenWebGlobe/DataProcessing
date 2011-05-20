c0ca3cda821024aa80e1ccf6de2b39db0fcf684a
Martin Christen <martin.christen@fhnw.ch>
2011-05-20 17:25:21 +0200

    calcextent working

--- source/apps/calcextent/main.cpp
+++ source/apps/calcextent/main.cpp
@@ -21,6 +21,11 @@
 #include "gdal.h"
 #include "ogr_api.h"
 #include "cpl_conv.h"
+#include "geo/MercatorQuadtree.h"
+#include "geo/CoordinateTransformation.h"
+#include "string/FilenameUtils.h"
+#include <iostream>
+
 
 //------------------------------------------------------------------------------
 
@@ -42,7 +47,42 @@ void exit_gdal()
 
 //------------------------------------------------------------------------------
 
-int main(void)
+int main(int argc, char *argv[])
 {
+   if (argc != 6)
+   {
+      std::cout << "Wrong arguments:\n";
+      std::cout << FilenameUtils::ExtractBaseFileName(std::string(argv[0]));
+      std::cout << " lng0 lat0 lng1 lat1 lod\n"; 
+      return 0;
+   }
+
+   double lng0, lat0, lng1, lat1;
+   int lod;
+
+   lng0 = atof(argv[1]);
+   lat0 = atof(argv[2]);
+   lng1 = atof(argv[3]);
+   lat1 = atof(argv[4]);
+   lod = atoi(argv[5]);
+
+   if (lng0 >= lng1) { std::cout << "error: lng1 must be greater than lng0\n"; return 0; }
+   if (lat0 >= lat1) { std::cout << "error: lat1 must be greater than lat0\n"; return 0; }
+   if (lod < 4) { std::cout << "error: level of detail must be atleast 4\n"; return 0; }
+
+
+   MercatorQuadtree* pQuadtree = new MercatorQuadtree();
+
+   int64 px0, py0, px1, py1, tx0, ty0, tx1, ty1;
+   pQuadtree->WGS84ToPixel(lng0, lat0, lod, px0, py1);
+   pQuadtree->WGS84ToPixel(lng1, lat1, lod, px1, py0);
+
+   std::cout << "Number of pixels in specified range: " << (px1-px0+1)*(py1-py0+1) << "\n";
+
+   pQuadtree->PixelToTileCoord(px0, py0, tx0, ty0);
+   pQuadtree->PixelToTileCoord(px1, py1, tx1, ty1);
+
+   std::cout << "Tile Coords: (" << tx0 << ", " << ty0 << ")-(" << tx1 << ", " << ty1 << ")\n";
+
    return 0;
 }
